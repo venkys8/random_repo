3e635b8beb338cdd6df41e34042e59e0310b8e96
venkys8 <venky8896@gmail.com>
2025-03-06 12:00:33 +0530

    adding new pg267_amd vip file description

---
+++ pg267_amd.sv
@@ -0,0 +1,9 @@
+
+
+o intro
+        - vip core, supports 3 version of axi(3,4,lite)
+        - unencrypted SV source[synthesizeable RTL?] ( difference b/w IP & VIP ?)
+        - m_axi, s_axi & pass-thru which acts as monitor. ( by using this VIP, we are generating transactions ?)
+
+o
+
